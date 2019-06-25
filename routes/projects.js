570dcf4ddef63448fa203cada8f583b67c8fb787
Yenni Tan <ytan@Yennis-MBP-2.fios-router.home>
2019-06-24 23:41:29 -0400

    add error handling for project not found

--- routes/projects.js
+++ routes/projects.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data.json');
+const httpStatus = require('http-status-codes');
 
 router.get('/:id', (req, res) => {
-    res.locals = data.projects[req.params.id];
-    res.render('project');
+    const id = req.params.id;
+    if (id >= data.projects.length) {
+        console.error(`Project ${id} does not exist.`)
+        return res.redirect('/').render('index', { projects: data.projects, notFound: true });
+    }
+    res.render('project', { project: data.projects[req.params.id]});
 });
 
 module.exports = router;
